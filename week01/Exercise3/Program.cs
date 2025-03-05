00415d451b0e83525e6140d77df17af8aebbb589
mamonjiharisoa <ronaldratsimbazafy@gmail.com>
2025-03-05 10:46:28 +0100

    add week 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+
+             int answer = 18;
+             int Your_answer ;
+             
+             Console.WriteLine("What is the magic number 18");
+             
+             while (true) {
+             
+              Console.Write("What is your guess ");
+             Your_answer = int.Parse(Console.ReadLine());
+
+             if (answer > Your_answer ){
+               Console.WriteLine("Higher");
+               }
+             else if ( Your_answer > answer){
+               Console.WriteLine("Lower"); 
+             }
+             else {
+               Console.WriteLine("you guessed it");
+                  break;
+             }
+
+            
+             } 
+             
+
+             
+ }        
 }
